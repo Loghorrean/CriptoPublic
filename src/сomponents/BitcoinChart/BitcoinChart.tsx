04b0893351ba5784a2b7b47bbc937c9fd66818ae
Loghorrean <Loghorrean>
2022-05-03 17:00:43 +0300

    Almost finished bitcoin chart

--- src/сomponents/BitcoinChart/BitcoinChart.tsx
+++ src/сomponents/BitcoinChart/BitcoinChart.tsx
@@ -19,19 +19,48 @@ const BitcoinChart = () => {
             clearInterval(intervalId)
         };
     });
-    const tableItems = () => {
+    const sellItems = () => {
         return bitcoinData.map((row, index) => {
             return <tr key={index}>
-                <td>Продажа</td>
-                <td>{row.binance.sell}</td>
-                <td>{row.coinbase.sell}</td>
-                <td>{row.kraken.sell}</td>
-                <td>{row.huobi.sell}</td>
-                <td>{row.kucoin.sell}</td>
-                <td>{row.ftx.sell}</td>
-                <td>{row.bitfinex.sell}</td>
-                <td>{row.gemini.sell}</td>
-
+                        <td>Продажа</td>
+                        <td>{row.binance.sell}</td>
+                        <td>{row.coinbase.sell}</td>
+                        <td>{row.kraken.sell}</td>
+                        <td>{row.huobi.sell}</td>
+                        <td>{row.kucoin.sell}</td>
+                        <td>{row.ftx.sell}</td>
+                        <td>{row.bitfinex.sell}</td>
+                        <td>{row.gemini.sell}</td>
+                    </tr>;
+        })
+    }
+    const buyItems = () => {
+        return bitcoinData.map((row, index) => {
+            return <tr key={index}>
+                <td>Покупка</td>
+                <td>{row.binance.buy}</td>
+                <td>{row.coinbase.buy}</td>
+                <td>{row.kraken.buy}</td>
+                <td>{row.huobi.buy}</td>
+                <td>{row.kucoin.buy}</td>
+                <td>{row.ftx.buy}</td>
+                <td>{row.bitfinex.buy}</td>
+                <td>{row.gemini.buy}</td>
+            </tr>;
+        })
+    }
+    const spotItems = () => {
+        return bitcoinData.map((row, index) => {
+            return <tr key={index}>
+                <td>Спот</td>
+                <td>{row.binance.spot}</td>
+                <td>{row.coinbase.spot}</td>
+                <td>{row.kraken.spot}</td>
+                <td>{row.huobi.spot}</td>
+                <td>{row.kucoin.spot}</td>
+                <td>{row.ftx.spot}</td>
+                <td>{row.bitfinex.spot}</td>
+                <td>{row.gemini.spot}</td>
             </tr>;
         })
     }
@@ -54,7 +83,9 @@ const BitcoinChart = () => {
                     </tr>
                     </thead>
                     <tbody>
-                        { tableItems() }
+                        { sellItems() }
+                        { buyItems() }
+                        { spotItems() }
                     </tbody>
                 </table>
             </div>
